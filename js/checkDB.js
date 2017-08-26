6e51b4184b131da86ad665c59c445d6c739e47b9
ilya <ilyanerd@gmail.com>
2017-08-26 11:57:53 +0300

    added profile image generation

--- js/checkDB.js
+++ js/checkDB.js
@@ -1,6 +1,7 @@
 var db = openDatabase("gallery", "1.0", "DB with pictures", 2 * 1024 * 1024);
 var checkGallery = "SELECT * FROM pictures";
 var checkAuthors = "SELECT * FROM authors";
+var checkOrders = "SELECT * FROM orders";
 
 if (!db) {
     alert("Failed to connect database")
@@ -11,10 +12,12 @@ if (!db) {
 
 db.transaction(function (trans) {
     trans.executeSql(checkGallery, [], function (tx, response) {
-        if(response.rows.length!==10) {
+        /*if(response.rows.length!==10) {
             dropPicturesTable();
             createPicturesTable();
-        }
+        }*/
+        dropPicturesTable();
+        createPicturesTable();
     }, function (tx, error) {
         createPicturesTable();
     });
@@ -22,14 +25,25 @@ db.transaction(function (trans) {
 
     trans.executeSql(checkAuthors, [],
         function (tx, response) {
-            if(response.rows.length!==7) {
+            /*if(response.rows.length!==7) {
                 dropAuthorsTable();
                 createAuthorsTable();
-            }
+            }*/
+            dropAuthorsTable();
+            createAuthorsTable();
         },
         function (tx, error) {
         createAuthorsTable();
     });
+
+    trans.executeSql(checkOrders, [],
+        function (tx, res) {
+            dropOrdersTable();
+            createOrders();
+    },
+        function (tx, err) {
+            createOrders();
+        });
 
 });
 
@@ -45,5 +59,12 @@ function dropAuthorsTable() {
     db = openDatabase("gallery", "1.0", "DB with pictures", 2 * 1024 * 1024);
     db.transaction(function (trans) {
         trans.executeSql("DROP TABLE authors", [], null, null);
+    });
+}
+
+function dropOrdersTable() {
+    db = openDatabase("gallery", "1.0", "DB with pictures", 2 * 1024 * 1024);
+    db.transaction(function (trans) {
+        trans.executeSql("DROP TABLE orders", [], null, null);
     });
 }
